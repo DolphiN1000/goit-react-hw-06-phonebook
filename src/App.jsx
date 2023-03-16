cf529877a6b55ba52f16b84154c390c6dba3ae18
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-03-16 03:29:56 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import {Provider} from 'react-redux';
+
 import Phonebook from 'components/Phonebook/Phonebook';
 
 export const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Phonebook />
-    </>
+    </Provider>
   );
 };
